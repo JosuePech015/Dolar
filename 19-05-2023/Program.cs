e02a2d2256e3a4b3694630978d02e218ea3db47d
OverlookHot <josuepech015@gmail.com>
2023-05-29 11:48:55 -0500

    Agregar archivos de proyecto.

---
+++ 19-05-2023/Program.cs
@@ -0,0 +1,14 @@
+﻿using _19_05_2023.Clases;
+using System;
+
+namespace _19_05_2023
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.PedirDatos();
+        }
+    }
+}
